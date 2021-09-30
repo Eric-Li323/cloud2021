c269f9d6c39b8c13b9dd234a6634cf6ada5e15dd
Eric-Li323 <liyonghuiwangyi@163.com>
2021-09-30 09:41:54 +0800

    config客户端配置

---
+++ cloud-config-client-3355/src/main/java/com/lyh/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.lyh.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lyh
+ * @date 2021/9/29
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
