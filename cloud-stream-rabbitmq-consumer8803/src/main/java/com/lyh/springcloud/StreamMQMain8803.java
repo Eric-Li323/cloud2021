f24c3847aff26a54d5fbf948cffdbe0a554a17b2
Eric-Li323 <liyonghuiwangyi@163.com>
2021-10-12 10:33:33 +0800

    Sleuth链路监控(借助zipkin)

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/lyh/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.lyh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lyh
+ * @date 2021/10/11
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
