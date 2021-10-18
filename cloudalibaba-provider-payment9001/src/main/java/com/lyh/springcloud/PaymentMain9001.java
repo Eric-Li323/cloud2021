2e5493784ae49f064eaff361cc7d9b9111144673
Eric-Li323 <liyonghuiwangyi@163.com>
2021-10-18 16:26:04 +0800

    nacos_服务提供者注册

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/lyh/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.lyh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lyh
+ * @date 2021/10/18
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/lyh/springcloud/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.lyh.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lyh
+ * @date 2021/10/18
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos registry, serverPort: "+serverPort+"\t id"+id;
+    }
+}
+
