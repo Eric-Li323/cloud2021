511a2f0135fd7be7b9168b422b7e08e0f48f43ac
Eric-Li323 <liyonghuiwangyi@163.com>
2021-10-19 15:25:38 +0800

    Nacos_消费者注册和负载均衡

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/lyh/springcloud/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package com.lyh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lyh
+ * @date 2021/10/18
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
