9fac6e6f1aac0906d9d16cc668dd8a4b634a7d6d
Eric-Li323 <liyonghuiwangyi@163.com>
2021-10-20 14:26:04 +0800

    nacos_配置中心用法

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/lyh/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.lyh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lyh
+ * @date 2021/10/20
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
