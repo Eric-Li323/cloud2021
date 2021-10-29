ab4fc42c346e6da48f1dfe381f548c9329ee0367
Eric-Li323 <liyonghuiwangyi@163.com>
2021-10-29 09:38:14 +0800

    Sentinel流控_线程数直接失败

--- cloudalibaba-sentinel-service8401/src/main/java/com/lyh/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lyh/springcloud/controller/FlowLimitController.java
@@ -3,6 +3,8 @@ package com.lyh.springcloud.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author lyh
  * @date 2021/10/28
@@ -12,6 +14,12 @@ public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA(){
+        //暂停毫秒
+        try{
+            TimeUnit.MILLISECONDS.sleep(8000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         return "-------testA";
     }
 
